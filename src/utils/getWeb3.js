36c2297f13681e6fc26a921cb0fc2cf9d30c0452
PJ Wickwire <pjpeterww@gmail.com>
2017-08-25 15:35:37 -0400

    fixed linting errors and added rules

--- src/utils/getWeb3.js
+++ src/utils/getWeb3.js
@@ -1,14 +1,15 @@
 import Web3 from 'web3';
 
-let getWeb3 = new Promise(function(resolve, reject) {
+const getWeb3 = new Promise((resolve) => {
   // Wait for loading completion to avoid race conditions with web3 injection timing.
-  window.addEventListener('load', function() {
-    var results;
+  window.addEventListener('load', () => {
+    let results;
+    let web3;
     // var web3 = window.web3
 
     // Checking if Web3 has been injected by the browser (Mist/MetaMask)
 
-    //so can't use the test net with meta mask so disable it
+    // so can't use the test net with meta mask so disable it
     // or at least just prevent this from happening in order to use the testrpc net
     // if (typeof web3 !== 'undefined') {
     //   // Use Mist/MetaMask's provider.
@@ -23,19 +24,19 @@ let getWeb3 = new Promise(function(resolve, reject) {
     //   resolve(results)
     //   console.log(results)
     // } else {
-      // Fallback to localhost if no web3 injection.
-      var provider = new Web3.providers.HttpProvider('http://localhost:8545')
-      web3 = new Web3(provider)
+    // Fallback to localhost if no web3 injection.
+    let provider = new Web3.providers.HttpProvider('http://localhost:8545');
+    web3 = new Web3(provider);
 
-      results = {
-        web3: web3
-      }
+    results = {
+      web3,
+    };
 
-      console.log('No web3 instance injected, using Local web3.');
+    console.log('No web3 instance injected, using Local web3.');
 
-      resolve(results)
+    resolve(results);
     // }
-  })
-})
+  });
+});
 
-export default getWeb3
+export default getWeb3;
